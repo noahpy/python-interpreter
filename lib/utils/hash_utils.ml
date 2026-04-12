d23a04de0ba633232143fb48369bb596440dfae1
noahpy <noschl@proton.me>
2026-04-12 12:19:55 +0200

    utils: add additional hash utils

--- lib/utils/hash_utils.ml
+++ lib/utils/hash_utils.ml
@@ -9,6 +9,12 @@ let add_variable (prog: program_state) (name: string) (value: expr) : unit =
 let get_variable (prog: program_state) (name: string) : expr option =
      Hashtbl.find_multi prog.variables name |> List.hd
 
+let get_nth_variable (prog: program_state) (name: string) (i: int): expr option = 
+    let l = Hashtbl.find_multi prog.variables name in List.nth l i
+
+let get_nth_variable_exn (prog: program_state) (name: string) (i: int): expr = 
+    let l = Hashtbl.find_multi prog.variables name in List.nth_exn l i
+
 let remove_variable (prog: program_state) (name: string) : unit =
     Hashtbl.remove_multi prog.variables name
 
@@ -20,6 +26,11 @@ let replace_variable (prog: program_state) (name: string) (value: expr) : unit =
       | [] -> reset_variable prog name value
       | h::r -> Hashtbl.set prog.variables ~key:name ~data:(value::r)
 
+let replace_nth_variable (prog: program_state) (name: string) (index: int) (value: expr) : unit =
+    match Hashtbl.find_multi prog.variables name with
+      | [] -> reset_variable prog name value
+      | l -> Hashtbl.set prog.variables ~key:name ~data:(List.mapi l ~f:(fun i v -> if i = index then value else v))
+
 let add_local_variable (prog: program_state) (name: string) (value: expr) : unit =
     match (Hashtbl.find prog.local_variables name) with
       | Some(v) -> replace_variable prog name value
@@ -27,3 +38,16 @@ let add_local_variable (prog: program_state) (name: string) (value: expr) : unit
 
 let remove_local_variabels (prog: program_state) : unit =
     Hashtbl.iter_keys prog.local_variables ~f:(fun key -> remove_variable prog key)
+
+let get_var_stacklen (prog: program_state) (name: string) : int = 
+    Hashtbl.find_multi prog.variables name |> List.length
+
+
+let rec resolve_reference (name: string) (index: int) (prog: program_state) : (string * int) =
+    let adjusted_i = (get_var_stacklen prog name) - index in
+    let res = get_nth_variable_exn prog name adjusted_i in
+    match res with
+      | Value(RefV(new_name, i)) ->
+              resolve_reference new_name i prog
+      | _ -> (name, adjusted_i)
+
